0d54dac5282c035557fea167db30b2495eaaa0b1
thomashin <dn2757@gmail.com>
2020-01-06 18:34:08 +0900

    config: apply 960 grid system
    https://960.gs/

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/css/960.css';
+import '@/assets/css/reset.css';
+import '@/assets/css/text.css';
 
 Vue.config.productionTip = false
 
