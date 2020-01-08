615c6de26c28e23e2b3211dec2d53e1d612ca6c5
thomashin <dn2757@gmail.com>
2020-01-08 17:56:02 +0900

    Make search bar component and table component frame

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input
+      type="text"
+      placeholder="Search"
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+input {
+  width: 100%;
+  height: 30px;
+  border-radius: 7px;
+  border: solid 0.7px #ced6d9;
+  background-color: #ffffff;
+}
+
+input::placeholder {
+  color: #aaaaaa;
+}
+</style>
---
+++ src/components/StandardTable.vue
@@ -0,0 +1,99 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th
+          v-for="(column, index) in columns"
+          :key="index"
+        >
+          {{ column }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(data, index) in datas"
+        :key="index"
+      >
+        <td
+          v-for="column in columns"
+          :key="column.title"
+        >
+          {{ data }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      columns: ['NO', 'ADDRESS', 'NAME', 'RECENT COMMIT TIMESTAMP', 'COMMISSION RATE'],
+      datas: [
+        {
+          title: 'test1',
+        },
+        {
+          title: 'test2',
+        },
+        {
+          title: 'test3',
+        },
+        {
+          title: 'test4',
+        },
+        {
+          title: 'test5',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+table {
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  border-spacing: 0px;
+  table-layout: auto;
+  width: 100%;
+}
+
+table td {
+  border-top: 1px solid #ddd;
+  text-align: center;
+}
+
+/* table td,
+table th {
+  padding: 10px;
+} */
+
+th {
+  height: 40px;
+  font-size: 9px;
+  font-weight: 300;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.89;
+  letter-spacing: normal;
+  text-align: center;
+  color: #005fa9;
+  background: #f8f8f8;
+}
+
+td {
+  height: 36px;
+  font-family: Roboto;
+  font-size: 10px;
+  font-weight: 300;
+  font-stretch: normal;
+  font-style: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #586064;
+}
+</style>
