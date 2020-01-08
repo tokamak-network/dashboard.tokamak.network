1fa3b5eef0d63c788080ea2256f1d3f9d9b091aa
thomashin <dn2757@gmail.com>
2020-01-08 17:55:11 +0900

    Make header container

---
+++ src/containers/HeaderContainer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="grid_8">
+      MAIN
+    </div>
+    <div class="header-container">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://your_url_here.html"
+      >Buy TON</a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://your_url_here.html"
+      >GUIDE</a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://your_url_here.html"
+      >FAQs</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header-container {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
