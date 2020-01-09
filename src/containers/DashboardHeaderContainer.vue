900841c276b718299ae367563672219b2a66b256
thomashin <dn2757@gmail.com>
2020-01-09 15:18:00 +0900

    Update dashboard header

--- src/containers/DashboardHeaderContainer.vue
+++ src/containers/DashboardHeaderContainer.vue
@@ -1,29 +1,54 @@
 <template>
   <div class="dashboard-header-container">
-    <wallet-info
-      :title="'Account'"
-      :info="account"
-    />
-    <wallet-info
-      :title="'Balance'"
-      :info="balance"
-    />
-    <wallet-info
-      :title="'Network'"
-      :info="networkId"
-    />
+    <div class="wallet-info-container">
+      <img
+        class="wallet-info-image"
+        :src="require(`@/assets/images/Account.svg`)"
+      >
+      <div>
+        <div class="wallet-info-title">
+          Account
+        </div>
+        <div class="wallet-info-content">
+          {{ account }}
+        </div>
+      </div>
+    </div>
+    <div class="wallet-info-container">
+      <img
+        class="wallet-info-image"
+        :src="require(`@/assets/images/Balance.svg`)"
+      >
+      <div>
+        <div class="wallet-info-title">
+          Balance
+        </div>
+        <div class="wallet-info-content">
+          {{ balance }}
+        </div>
+      </div>
+    </div>
+    <div class="wallet-info-container">
+      <img
+        class="wallet-info-image"
+        :src="require(`@/assets/images/Network.svg`)"
+      >
+      <div>
+        <div class="wallet-info-title">
+          Network
+        </div>
+        <div class="wallet-info-content">
+          {{ networkId }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 
-import WalletInfo from '@/components/WalletInfo.vue';
-
 export default {
-  components: {
-    'wallet-info': WalletInfo,
-  },
   computed: mapState([
     'account',
     'networkId',
@@ -34,7 +59,53 @@ export default {
 
 <style>
 .dashboard-header-container {
+  width: 100%;
   display: flex;
-  justify-content: space-between;
+}
+
+.wallet-info-container {
+  width: 33.3%;
+  display: flex;
+  align-items: center;
+  height: 71px;
+  border-radius: 6px;
+  border: solid 0.7px #ced6d9;
+  background-color: #ffffff;
+}
+
+.wallet-info-container:first-child {
+  margin-right: 8px;
+}
+
+.wallet-info-container:last-child {
+  margin-left: 8px;
+}
+
+.wallet-info-title {
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 500;
+  font-stretch: normal;
+  margin-bottom: 2px;
+  font-style: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #124b71;
+}
+
+.wallet-info-content {
+  font-family: Roboto;
+  font-size: 12.5px;
+  line-height: 1.08;
+  letter-spacing: 0.2px;
+  text-align: left;
+  color: #586064;
+  padding-right: 17px;
+  word-break: break-all;
+}
+
+.wallet-info-image {
+  padding-left: 16.9px;
+  padding-right: 16.9px;
 }
 </style>
