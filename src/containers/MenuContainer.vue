ca5348b62faf6d8ba31c5f379c063d70b99680a3
thomashin <dn2757@gmail.com>
2020-01-10 17:22:25 +0900

    Update menu layout

--- src/containers/MenuContainer.vue
+++ src/containers/MenuContainer.vue
@@ -3,99 +3,90 @@
     <div
       class="menu-title-container"
       :class="{ 'menu-selected': $route.path === '/dashboard'}"
+      @click="clickMenu('dashboard')"
     >
       <img
         class="menu-image"
-        src="@/assets/images/DashboardMenu.svg"
+        :class="{ 'menu-button-selected': $route.path === '/dashboard'}"
+        src="@/assets/images/DashboardMenu.png"
         width="10.7"
         height="9.7"
       >
-      <router-link
-        class="menu-title"
-        to="/dashboard"
-      >
-        Dashboard
-      </router-link>
+      Dashboard
     </div>
     <div
+      style="margin-top: 16px;"
       class="menu-title-container"
       :class="{ 'menu-selected': $route.path !== '/dashboard'}"
+      @click="clickMenu('operators')"
     >
       <img
-        class="menu-image"
-        src="@/assets/images/StakingMenu.svg"
+        class="menu-image menu-button-clicked"
+        :class="{ 'menu-button-selected': $route.path === '/dashboard'}"
+        src="@/assets/images/StakingMenu.png"
         width="13"
         height="13"
       >
-      <router-link
-        class="menu-title"
-        to="/operators"
-      >
-        Staking
-      </router-link>
+      Staking
     </div>
     <div
-      class="menu-title-container"
+      class="sub-menu-title-container"
       :class="{ 'menu-selected': $route.path === '/operators'}"
+      @click="clickMenu('operators')"
     >
-      <router-link
-        class="sub-menu-title"
-        to="/operators"
-      >
+      <div class="sub-menu-title">
         Operator List
-      </router-link>
+      </div>
     </div>
     <div
-      class="menu-title-container"
+      class="sub-menu-title-container"
       :class="{ 'menu-selected': $route.path === '/staking'}"
+      @click="clickMenu('staking')"
     >
-      <router-link
-        class="sub-menu-title"
-        to="/staking"
-      >
+      <div class="sub-menu-title">
         My Staking
-      </router-link>
+      </div>
     </div>
     <div
-      class="menu-title-container"
+      class="sub-menu-title-container"
       :class="{ 'menu-selected': $route.path === '/history'}"
+      @click="clickMenu('history')"
     >
-      <router-link
-        class="sub-menu-title"
-        to="/history"
-      >
+      <div class="sub-menu-title">
         History
-      </router-link>
+      </div>
     </div>
-    <div v-if="$route.path === path" />
   </div>
 </template>
 
 <script>
+export default {
+  methods: {
+    clickMenu (path) {
+      this.$router.push(path);
+    },
+  },
+};
 </script>
 
 <style>
-.menu-selected {
-  background: #f7f8f9;
-}
-
 .menu-container {
+  padding-top: 24px;
+  padding-bottom: 24px;
   background: #ffffff;
-  width: 235px;
-  height: 100%;
+  width: 260px;
+  min-height: 100%;
+  border-radius: 7px;
   border: solid 0.7px #ced6d9;
-  padding-bottom: 1px;
 }
 
 .menu-title-container {
+  display: flex;
+  align-items: center;
   padding: 10px;
   padding-left: 16px;
-}
-
-.menu-title {
   text-decoration: none;
   padding-left: 8px;
-  font-family: Roboto;
   font-size: 16px;
   font-weight: 500;
   font-stretch: normal;
@@ -103,18 +94,44 @@
   line-height: 1.67;
   letter-spacing: normal;
   text-align: left;
-  color: #3e3e3e;
+  color: #c4c4c4;
 }
 
-.sub-menu-title {
-  padding-left: 24px;
+.menu-title-container svg {
+  fill: red;
+}
+
+.menu-image {
+  margin-left: 5px;
+  margin-right: 10px;
+}
+
+.sub-menu-title-container {
+  padding: 10px;
+  padding-left: 36px;
   text-decoration: none;
-  font-family: Roboto;
-  font-size: 15px;
+  font-size: 14px;
   font-stretch: normal;
   font-style: normal;
   letter-spacing: normal;
   text-align: left;
-  color: #5d5d5d;
+  color: #c4c4c4;
+}
+
+.menu-title-container:hover {
+  cursor: pointer;
+}
+
+.sub-menu-title-container:hover {
+  cursor: pointer;
+}
+
+.menu-selected {
+  background: #f7f8f9;
+  color: #3e3e3e;
+}
+
+.menu-button-selected {
+  filter: invert(75%);
 }
 </style>
