7d9cc5593f8f24513ab188ed1d92100c5fa92467
thomashin <dn2757@gmail.com>
2020-01-15 13:33:30 +0900

    Make history layout

---
+++ src/layouts/HistoryLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="history-layout">
+    <standard-table
+      :columns="['ADDRESS', 'NAME', 'COMMIT TIMESTAMP', 'COMMISSION RATE']"
+      :datas="datas"
+    />
+  </div>
+</template>
+
+<script>
+import StandardTable from '@/components/StandardTable.vue';
+
+export default {
+  components: {
+    'standard-table': StandardTable,
+  },
+  data () {
+    return {
+      datas: [
+        {
+          '1': 1000,
+          '2': 20000,
+          '3': 3000,
+          '4': 4000,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.history-layout {
+  margin-bottom: -19.5px;
+  border-left: solid 0.7px #ced6d9;
+  border-right: solid 0.7px #ced6d9;
+  background-color: #ffffff;
+}
+</style>
