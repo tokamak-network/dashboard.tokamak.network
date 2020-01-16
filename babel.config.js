7bc9314f809c34a443a716d740fcd6b841a681bc
thomashin <dn2757@gmail.com>
2020-01-16 18:05:37 +0900

    Fix babel module bug

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,9 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
+  env: {
+    'test': {
+      'presets': [['@babel/env', { 'targets': { 'node': 'current' } }]],
+    },
+  },
 };
