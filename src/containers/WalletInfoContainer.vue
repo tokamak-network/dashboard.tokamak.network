55cc2591a94e145c66199196f99a68247f741a8e
thomashin <dn2757@gmail.com>
2020-01-21 17:26:35 +0900

    Add toggle switch component and use it

--- src/containers/WalletInfoContainer.vue
+++ src/containers/WalletInfoContainer.vue
@@ -1,44 +1,84 @@
 <template>
   <div class="wallet-info-container">
-    <div class="wallet-info">
-      <img
-        class="wallet-info-image"
-        :src="require(`@/assets/images/Account.svg`)"
-      >
-      <div>
-        <div class="wallet-info-title">
-          Account
+    <div class="wallet-basic-info-container">
+      <div class="wallet-info">
+        <img
+          class="wallet-info-image"
+          :src="require(`@/assets/images/Account.svg`)"
+        >
+        <div>
+          <div class="wallet-info-title">
+            Account
+          </div>
+          <div class="wallet-info-content">
+            {{ account }}
+          </div>
         </div>
-        <div class="wallet-info-content">
-          {{ account }}
+      </div>
+      <div class="wallet-info">
+        <img
+          class="wallet-info-image"
+          :src="require(`@/assets/images/Network.svg`)"
+        >
+        <div>
+          <div class="wallet-info-title">
+            Network
+          </div>
+          <div class="wallet-info-content">
+            {{ networkId }}
+          </div>
         </div>
       </div>
     </div>
-    <div class="wallet-info">
-      <img
-        class="wallet-info-image"
-        :src="require(`@/assets/images/Balance.svg`)"
-      >
-      <div>
-        <div class="wallet-info-title">
-          Balance
+    <div class="wallet-assets-info-container">
+      <div class="wallet-assets-info">
+        <img
+          class="wallet-info-image"
+          :src="require(`@/assets/images/Balance.svg`)"
+        >
+        <div>
+          <div class="wallet-info-title">
+            Assets
+          </div>
         </div>
-        <div class="wallet-info-content">
+      </div>
+      <div class="wallet-assets-header">
+        <div class="wallet-header">
+          ASSET
+        </div>
+        <div class="wallet-header">
+          AMOUNT
+        </div>
+        <div class="wallet-header">
+          UNLOCK
+        </div>
+      </div>
+      <div class="wallet-assets-content">
+        <div class="wallet-content">
+          TON
+        </div>
+        <div class="wallet-content">
           {{ balance }}
         </div>
-      </div>
-    </div>
-    <div class="wallet-info">
-      <img
-        class="wallet-info-image"
-        :src="require(`@/assets/images/Network.svg`)"
-      >
-      <div>
-        <div class="wallet-info-title">
-          Network
+        <div class="wallet-content">
+          <toggle-switch
+            :checked="tonUnlocked"
+            :func="tonUnlocked ? lockTON : unlockTON"
+          />
         </div>
-        <div class="wallet-info-content">
-          {{ networkId }}
+      </div>
+      <div class="wallet-assets-content">
+        <div class="wallet-content">
+          WTON
+        </div>
+        <div class="wallet-content">
+          {{ balance }}
+        </div>
+        <div class="wallet-content">
+          <toggle-switch
+            :checked="wtonUnlocked"
+            :func="wtonUnlocked ? lockWTON : unlockWTON"
+          />
         </div>
       </div>
     </div>
@@ -48,12 +88,40 @@
 <script>
 import { mapState } from 'vuex';
 
+import ToggleSwitch from '@/components/ToggleSwitch.vue';
+
 export default {
+  components: {
+    'toggle-switch': ToggleSwitch,
+  },
+  data () {
+    return {
+      tonUnlocked: true,
+      wtonUnlocked: false,
+    };
+  },
   computed: mapState([
     'account',
     'networkId',
     'balance',
   ]),
+  beforeCreate () {
+    // TODO: check if ton and wton is unlocked.
+  },
+  methods: {
+    lockTON () {
+      alert('1');
+    },
+    unlockTON () {
+      alert('2');
+    },
+    lockWTON () {
+      alert('3');
+    },
+    unlockWTON () {
+      alert('4');
+    },
+  },
 };
 </script>
 
@@ -64,8 +132,25 @@ export default {
   padding-bottom: 16px;
 }
 
+.wallet-basic-info-container {
+  width: 40%;
+  margin-right: 4px;
+}
+
+.wallet-assets-info-container {
+  padding-top: 16px;
+  margin-left: 4px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+  min-height: 100%;
+  border-radius: 6px;
+  border: solid 0.7px #ced6d9;
+  background-color: #ffffff;
+}
+
 .wallet-info {
-  width: 33.3%;
   display: flex;
   align-items: center;
   height: 74px;
@@ -75,11 +160,48 @@ export default {
 }
 
 .wallet-info:first-child {
-  margin-right: 8px;
+  margin-bottom: 8px;
 }
 
 .wallet-info:last-child {
-  margin-left: 8px;
+  margin-top: 8px;
+}
+
+.wallet-assets-info {
+  display: flex;
+  align-items: center;
+}
+
+.wallet-assets-header {
+  margin-top: 12px;
+  margin-bottom: 8px;
+  display: flex;
+  width: 100%;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+}
+
+.wallet-header {
+  margin-top: 4px;
+  margin-bottom: 4px;
+  width: 33.3%;
+  text-align: center;
+  color: #586064;
+  font-size: 11px;
+}
+
+.wallet-assets-content {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+
+.wallet-content {
+  margin-top: 4px;
+  margin-bottom: 2px;
+  width: 33.3%;
+  text-align: center;
+  font-size: 13px;
 }
 
 .wallet-info-title {
