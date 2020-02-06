210afe24d41a7a17c8d6f1db7780ecb0e1626dd3
thomashin <dn2757@gmail.com>
2020-02-06 16:18:47 +0900

    Make vue filter

---
+++ src/filters/index.js
@@ -0,0 +1,7 @@
+export function addressSlicer (address = '') {
+  if (address.length < 11) {
+    return address;
+  }
+
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import router from './router';
 import store from './store';
 
 import '@/assets/css/960.css';
-import '@/assets/css/reset.css';
+// import '@/assets/css/reset.css';
 import '@/assets/css/text.css';
+import * as filters from '@/filters';
 
 Vue.config.productionTip = false;
+
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key]);
+});
 
 new Vue({
   router,
