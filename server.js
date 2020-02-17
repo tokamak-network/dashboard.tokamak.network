bdcec268f4c4bfc7e7da696e8057aa27ecaac621
thomashin <dn2757@gmail.com>
2020-02-17 20:32:20 +0900

    Add api functions

--- server.js
+++ server.js
@@ -10,15 +10,31 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (_, res) {
-  const homeDir = require('os').homedir();
-  const plsStakingDir = path.join(homeDir, '.pls.staking');
+const homeDir = require('os').homedir();
+const plsStakingDir = path.join(homeDir, '.pls.staking');
 
+app.get('/', function (_, res) {
   const managers = JSON.parse(fs.readFileSync(`${plsStakingDir}/managers.json`));
   const rootchains = JSON.parse(fs.readFileSync(`${plsStakingDir}/rootchains.json`));
 
   return res.status(200).json({
     managers,
+    rootchains,
+  });
+});
+
+app.get('/managers', function (_, res) {
+  const managers = JSON.parse(fs.readFileSync(`${plsStakingDir}/managers.json`));
+
+  return res.status(200).json({
+    managers,
+  });
+});
+
+app.get('/rootchains', function (_, res) {
+  const rootchains = JSON.parse(fs.readFileSync(`${plsStakingDir}/rootchains.json`));
+
+  return res.status(200).json({
     rootchains,
   });
 });
@@ -32,6 +48,23 @@ app.get('/history/:user', function (req, res) {
   });
 });
 
+app.post('/managers', function (req, res) {
+  const managers = req.body.managers;
+  fs.writeFileSync(`${plsStakingDir}/managers.json`, JSON.stringify(managers));
+
+  return res.status(200).json({});
+});
+
+app.post('/rootchains', function (req, res) {
+  let rootchains = JSON.parse(fs.readFileSync(`${plsStakingDir}/rootchains.json`));
+  if (!rootchains) rootchains = [];
+
+  rootchains.push(req.body.rootchain);
+  fs.writeFileSync(`${plsStakingDir}/rootchains.json`, JSON.stringify(rootchains));
+
+  return res.status(200).json({});
+});
+
 app.post('/history/:user', function (req, res) {
   const user = (req.params.user).toLowerCase();
   const history = req.body.history;
@@ -40,6 +73,13 @@ app.post('/history/:user', function (req, res) {
   return res.status(200).json({
     history: userHistory,
   });
+});
+
+app.delete('/', function (_, res) {
+  fs.writeFileSync(`${plsStakingDir}/rootchains.json`, JSON.stringify([]));
+  fs.writeFileSync(`${plsStakingDir}/managers.json`, JSON.stringify({}));
+
+  return res.status(200).json({});
 });
 
 app.listen(9000, function () {
