4a8d0e6bae9cee90e51bccbeda5cfdf4fa628844
thomashin <dn2757@gmail.com>
2020-02-24 13:46:32 +0900

    Make base URL dynamic

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function createInstance () {
   return axios.create({
-    baseURL: 'http://127.0.0.1:9000',
+    baseURL: `http://${window.location.hostname}:9000`,
   });
 }
 
