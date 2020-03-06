e058ff036f00bafda3094546f752f8b26a49487b
thomashin <dn2757@gmail.com>
2020-03-06 13:36:58 +0900

    Update opreator info layout

--- src/layouts/OperatorInfoLayout.vue
+++ src/layouts/OperatorInfoLayout.vue
@@ -1,12 +1,12 @@
 <template>
-  <div style="display: flex;">
+  <div class="column">
     <operator-info-container
       :operator="operator"
       style="flex: 1; margin-right: 4px;"
-    />g
+    />
     <delegate-manager-container
-      style="flex: 1; margin-left: 4px;"
       :operator="operator"
+      style="flex: 1; margin-left: 4px;"
       @refresh="refreshOperator"
     />
   </div>
@@ -53,88 +53,7 @@ export default {
 </script>
 
 <style scoped>
-.operator-info-layout {
-  height: 100%;
-  border-radius: 6px;
-  border: solid 0.7px #ced6d9;
-  background-color: #ffffff;
-}
-.operator-info-container {
+.column{
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100px;
-  margin-left: 16px;
-  margin-right: 16px;
-}
-
-.operator-info-image {
-  margin-left: 16px;
-  margin-right: 16px;
-  width: 65px;
-  height: 65px;
-}
-
-.operator-name-label {
-  font-family: Roboto;
-  font-size: 16px;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  letter-spacing: normal;
-  text-align: left;
-}
-
-.operator-info-label {
-  width: 60px;
-  margin-right: 4px;
-  font-size: 12px;
-  font-weight: 500;
-  text-align: left;
-  color: #586064;
-}
-
-.operator-info-content {
-  flex: 1;
-  word-break: break-all;
-  padding-right: 24px;
-  font-size: 12px;
-  font-weight: 200;
-  text-align: left;
-  color: #586064;
-}
-
-.operator-info-detailed-label {
-  flex: 1;
-  font-size: 12px;
-  font-weight: 300;
-  font-stretch: normal;
-  font-style: normal;
-  letter-spacing: normal;
-  text-align: left;
-  color: #676767;
-  padding-left: 20px;
-}
-
-.operator-info-detailed-content {
-  font-size: 12px;
-  font-weight: 300;
-  font-stretch: normal;
-  font-style: normal;
-  letter-spacing: normal;
-  text-align: right;
-  color: #586064;
-  padding-right: 20px;
-}
-
-.operator-info-detailed-container {
-  width: 100%;
-}
-
-.operator-info-detailed {
-  display: flex;
-  align-items: center;
-  height: 36px;
-  border-top: solid 0.7px #ced6d9;
 }
 </style>
