b559c478d38824d96913c7bad6af3ba66f93fd61
thomashin <dn2757@gmail.com>
2020-03-25 16:19:51 +0900

    Delete truffle contract library

--- src/helpers/Contract.js
+++ src/helpers/Contract.js
@@ -1,19 +1,4 @@
-import TruffleContract from '@truffle/contract';
-
 import store from '@/store/index.js';
-
-export async function createTruffleContract (abi, address) {
-  const web3 = store.state.web3;
-  let contract;
-  // TODO: fix
-  try {
-    contract = TruffleContract({ abi });
-    contract.setProvider(web3.currentProvider);
-    return await contract.at(address);
-  } catch (e) {
-    //
-  }
-}
 
 export async function createWeb3Contract (abi, address, from) {
   try {
