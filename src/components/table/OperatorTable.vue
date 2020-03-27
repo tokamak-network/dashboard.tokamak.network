7b86b5cbe737254160cbb7b52ff6bb233e5e4db7
thomashin <dn2757@gmail.com>
2020-03-27 21:05:04 +0900

    Update operator list layout

---
+++ src/components/table/OperatorTable.vue
@@ -0,0 +1,160 @@
+<template>
+  <table class="operator-table">
+    <thead>
+      <tr>
+        <th class="text-center">#</th>
+        <th class="pointer text-center" @click="orderBy('name')">{{ withArrow('name', 'Operator Name') }}</th>
+        <th class="pointer text-right" @click="orderBy('operator')">{{ withArrow('operator', 'Operator') }}</th>
+        <th class="pointer text-right" @click="orderBy('rootchain')">{{ withArrow('rootchain', 'RootChain') }}</th>
+        <th class="pointer text-right" @click="orderBy('userStaked')">{{ withArrow('userStaked', 'Staked') }}</th>
+        <th class="pointer text-right" @click="orderBy('totalStaked')">{{ withArrow('totalStaked', 'Total Staked') }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(operator, index) in orderedOperators" :key="operator.rootchain">
+        <td class="text-center">{{ index }}</td>
+        <td class="clickable text-center" @click="viewDetailedOperator(operator)"><span class="pointer name">{{ operator.name }}</span></td>
+        <td class="text-right">{{ operator.address | hexSlicer }}</td>
+        <td class="text-right">{{ operator.rootchain | hexSlicer }}</td>
+        <td class="text-right">{{ operator.userStaked | convertedTONFromWTON }} </td>
+        <td class="text-right">{{ operator.totalStaked | convertedTONFromWTON }} </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { orderBy } from 'lodash';
+import { mapState } from 'vuex';
+
+export default {
+  data () {
+    return {
+      from: 'name',
+      order: 'desc',
+    };
+  },
+  computed: {
+    ...mapState([
+      'operators',
+    ]),
+    orderedOperators () {
+      switch (this.from) {
+      case 'name':
+        return orderBy(this.operators, (operator) => operator.name, [this.order]);
+      case 'operator':
+        return orderBy(this.operators, (operator) => operator.address, [this.order]);
+      case 'rootchain':
+        return orderBy(this.operators, (operator) => operator.rootchain, [this.order]);
+      case 'userStaked':
+        return orderBy(this.operators, (operator) => operator.userStaked.toNumber(), [this.order]);
+      case 'totalStaked':
+        return orderBy(this.operators, (operator) => operator.totalStaked.toNumber(), [this.order]);
+
+      default:
+        return [];
+      }
+    },
+    withArrow () {
+      return (from, label) => {
+        if (this.from === from) {
+          return this.order === 'desc' ? `${label} ↑` : `${label} ↓`;
+        }
+        return label;
+      };
+    },
+  },
+  methods: {
+    orderBy (from) {
+      if (this.from === from) {
+        this.order = this.changedOrder();
+      } else {
+        this.from = from;
+        this.order = 'desc';
+      }
+    },
+    changedOrder () {
+      return this.order === 'desc' ? 'asc' : 'desc';
+    },
+    viewDetailedOperator (operator) {
+      const rootchain = operator.rootchain;
+      this.$router.push(`/operators/${rootchain.toLowerCase()}`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.operator-table {
+  width: 100%;
+  table-layout: auto;
+  border-collapse: collapse;
+  border-spacing: 0;
+  border: solid 0.7px #ced6d9;
+  background: #ffffff;
+}
+
+.operator-table td, .operator-table th {
+  border-bottom: solid 0.5px #dce2e5;
+  /* border: 1px solid #555561; */
+  /* padding: 8px; */
+}
+
+tbody tr:hover {
+  background-color: #f8f8f8;
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+tbody .clickable {
+  font-weight: bolder;
+  text-decoration: underline;
+}
+
+.operator-table th {
+  text-align: left;
+}
+
+.operator-table td {
+  text-align: left;
+}
+
+.operator-table .text-center {
+  text-align: center;
+}
+
+.operator-table .text-right {
+  text-align: right;
+}
+
+th {
+  padding: 6px;
+  background-color: #f6f8f9;
+  font-family: Roboto;
+  font-size: 9px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #7e8d93;
+}
+
+td {
+  padding: 12px;
+  font-family: Roboto;
+  font-size: 10px;
+  font-weight: 300;
+  font-stretch: normal;
+  font-style: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #161819;
+}
+
+.name {
+  word-break: break-all;
+}
+</style>
--- src/layouts/OperatorListLayout.vue
+++ src/layouts/OperatorListLayout.vue
@@ -1,43 +1,18 @@
 <template>
   <div>
-    <div class="operator-list-layout table-container">
-      <base-table
-        :type="'operator'"
-        :columns="[
-          {
-            name: 'OPERATOR',
-            key: 'name',
-          },
-          {
-            name: 'ADDRESS',
-            key: 'address',
-          },
-          {
-            name: 'COMMIT TIMESTAMP',
-            key: 'recentCommitTimestamp',
-          },
-          {
-            name: 'TOTAL STAKE',
-            key: 'totalStaked',
-          },
-        ]"
-        :datas="operators"
-        :rounded="true"
-        :clickable="true"
-        @tableDataClicked="viewOperator"
-      />
+    <div class="operator-list-layout">
+      <operator-table />
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
-
-import BaseTable from '@/components/BaseTable.vue';
+import OperatorTable from '@/components/table/OperatorTable.vue';
 
 export default {
   components: {
-    'base-table': BaseTable,
+    'operator-table': OperatorTable,
   },
   data () {
     return {
@@ -58,8 +33,8 @@ export default {
 
 <style scoped>
 .operator-list-layout {
-  border: solid 1px #ced6d9;
+  /* border: solid 1px #ced6d9;
   background-color: #ffffff;
-  border-radius: 6px;
+  border-radius: 6px; */
 }
 </style>
