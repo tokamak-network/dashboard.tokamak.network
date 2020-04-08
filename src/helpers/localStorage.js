3e379cdf60207ff4f4246109ba24e30b95ee1141
thomashin <dn2757@gmail.com>
2020-04-08 16:20:16 +0900

    Update localstorage (use lowdb)

--- src/helpers/localStorage.js
+++ src/helpers/localStorage.js
@@ -1,22 +1,23 @@
 import store from '@/store/index.js';
-
-// used only in development
-export function clearPendingTransactions () {
-  window.localStorage.clear();
-}
+import low from 'lowdb';
+import LocalStorage from 'lowdb/adapters/LocalStorage';
+const adapter = new LocalStorage('db');
+const db = low(adapter);
 
 export function getPendingTransactions () {
   const user = store.state.user;
   const networkId = store.state.networkId;
-  const pendingTransactions = window.localStorage.getItem(`pending-transactions-${user}-${networkId}`);
-  if (pendingTransactions === null) {
+  const pendingTransactions = db.get(`pending-transactions-${user}-${networkId}`).value();
+  if (pendingTransactions) {
+    return pendingTransactions;
+  } else {
     return [];
   }
-  return JSON.parse(pendingTransactions);
 }
 
 export function setPendingTransactions (pendingTransactions=[]) {
   const user = store.state.user;
   const networkId = store.state.networkId;
-  window.localStorage.setItem(`pending-transactions-${user}-${networkId}`, JSON.stringify(pendingTransactions));
+  db.set(`pending-transactions-${user}-${networkId}`, pendingTransactions)
+    .write();
 }
