fd8a831c2c23f8a719427bb36490604755a64ba5
thomashin <dn2757@gmail.com>
2020-04-12 16:17:47 +0900

    Configuration to measue performance

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import '@/assets/css/960.css';
 import '@/assets/css/text.css';
 import * as filters from '@/filters';
 
-Vue.config.productionTip = false;
+// https://vuedose.tips/tips/measure-runtime-performance-in-vue-js-apps
+const isDev = process.env.NODE_ENV !== 'production';
+Vue.config.performance = isDev;
 
 Object.keys(filters).forEach(key => {
   Vue.filter(key, filters[key]);
