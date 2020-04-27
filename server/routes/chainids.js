ce923d87cb0dfcda6e7eab2a7d39056c97c95b84
thomashin <dn2757@gmail.com>
2020-04-27 19:05:14 +0900

    Return chain ids including already known chain ids #55

--- server/routes/chainids.js
+++ server/routes/chainids.js
@@ -1,11 +1,15 @@
+// https://chainid.network/chains.json
+const chains = require('../../src/helpers/chainids.json');
+const chainIds = chains.map(chain => chain.chainId);
+
 const GET = (db) => {
   try {
-    const chainIds = db
+    const chainIdsOfOperator = db
       .get('operators')
       .map('chainId')
       .value();
 
-    return Promise.resolve(chainIds);
+    return Promise.resolve(chainIds.concat(chainIdsOfOperator));
   } catch (err) {
     throw err;
   }
