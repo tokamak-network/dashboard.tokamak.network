8d5f696dd76d1460019708c4bbd8ae319d3a9637
thomashin <dn2757@gmail.com>
2020-04-29 18:42:55 +0900

    Set dynamic port using config library

--- server/index.js
+++ server/index.js
@@ -8,9 +8,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const path = require('path');
 const args = process.argv.slice(2);
-switch (args[0]) {
+const network = args[0] || 'mainnet';
+const path = require('path');
+switch (network) {
 case 'rinkeby':
   app.use('/avatars', express.static(path.join(__dirname, '..', 'db', 'rinkeby', 'avatars')));
   break;
@@ -25,6 +26,8 @@ default:
 const routes = require('./routes');
 app.use('/', routes);
 
-app.listen(9000, () => {
-  console.log('Server listening on http://localhost:9000');
+const config = require('config');
+const port = config.get(`port_${network}`);
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
 });
