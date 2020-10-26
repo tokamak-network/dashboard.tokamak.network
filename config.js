d667447984c988b9971204a769a2d5a248ecfd89
Lakmi <lakmi94@gmail.com>
2020-10-26 11:05:57 +0900

    config file

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     'network': '1',
   },
   'rinkeby': {
-    'baseURL': 'http://127.0.0.1:9000',
+    'baseURL': 'https://dashboard-api.tokamak.network/rinkeby',
     'prefixTransactionHash': 'https://rinkeby.etherscan.io/tx/',
     'prefixAddress': 'https://rinkeby.etherscan.io/address/',
     'network': '4',
@@ -29,7 +29,7 @@ export function getConfig () {
   case 'development':
     return config.development;
   default:
-    return config.mainnet;
+    return config.rinkeby;
   }
 }
 
