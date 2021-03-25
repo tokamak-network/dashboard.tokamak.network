36c2fc810eca57ed4eae936e72b221c241d67957
Lakmi <lakmi94@gmail.com>
2021-03-25 20:51:24 +0900

    updated

--- src/containers/NewHeaderContainer.vue
+++ src/containers/NewHeaderContainer.vue
@@ -75,61 +75,61 @@ export default {
     showPopUp () {
       this.showModel = !this.showModel;
     },
-    async login (){
-      if (this.loading) return;
-      this.loading = true;
-      await this.useMetamask();
-      this.loading = false;
-    },
-    async useMetamask () {
-      try {
-        const web3 = await this.metamask();
-        await this.$store.dispatch('signIn', web3);
+    // async login (){
+    //   if (this.loading) return;
+    //   this.loading = true;
+    //   await this.useMetamask();
+    //   this.loading = false;
+    // },
+    // async useMetamask () {
+    //   try {
+    //     const web3 = await this.metamask();
+    //     await this.$store.dispatch('signIn', web3);
 
-        window.ethereum.on('chainIdChanged', (chainId) => {
-          this.$store.dispatch('logout');
-          this.$router.replace({
-            path: '/',
-            query: { network: this.$route.query.network },
-          }).catch(err => {});
-        });
-        window.ethereum.on('accountsChanged', (account) => {
-          this.$store.dispatch('logout');
-          this.$router.replace({
-            path: '/',
-            query: { network: this.$route.query.network },
-          }).catch(err => {});
+    //     window.ethereum.on('chainIdChanged', (chainId) => {
+    //       this.$store.dispatch('logout');
+    //       this.$router.replace({
+    //         path: '/',
+    //         query: { network: this.$route.query.network },
+    //       }).catch(err => {});
+    //     });
+    //     window.ethereum.on('accountsChanged', (account) => {
+    //       this.$store.dispatch('logout');
+    //       this.$router.replace({
+    //         path: '/',
+    //         query: { network: this.$route.query.network },
+    //       }).catch(err => {});
 
-        });
-      } catch (e) {
-        alert(e.message);
-      }
-    },
-    async metamask () {
-      let provider;
-      if (typeof window.ethereum !== 'undefined') {
-        try {
-          await window.ethereum.enable();
-          provider = window.ethereum;
-        } catch (e) {
-          if (e.stack.includes('Error: User denied account authorization')) {
-            throw new Error('User denied account authorization');
-          } else {
-            throw new Error(e.message);
-          }
-        }
-      } else if (window.web3) {
-        provider = window.web3.currentProvider;
-      } else {
-        throw new Error('No web3 provider detected');
-      }
+    //     });
+    //   } catch (e) {
+    //     alert(e.message);
+    //   }
+    // },
+    // async metamask () {
+    //   let provider;
+    //   if (typeof window.ethereum !== 'undefined') {
+    //     try {
+    //       await window.ethereum.enable();
+    //       provider = window.ethereum;
+    //     } catch (e) {
+    //       if (e.stack.includes('Error: User denied account authorization')) {
+    //         throw new Error('User denied account authorization');
+    //       } else {
+    //         throw new Error(e.message);
+    //       }
+    //     }
+    //   } else if (window.web3) {
+    //     provider = window.web3.currentProvider;
+    //   } else {
+    //     throw new Error('No web3 provider detected');
+    //   }
 
-      if (provider.networkVersion !== getConfig().network) {
-        throw new Error(`Please connect to the '${this.$options.filters.nameOfNetwork(getConfig().network)}' network`);
-      }
+    //   if (provider.networkVersion !== getConfig().network) {
+    //     throw new Error(`Please connect to the '${this.$options.filters.nameOfNetwork(getConfig().network)}' network`);
+    //   }
 
-      return new Web3(provider);
-    },
+    //   return new Web3(provider);
+    // },
     toMainPage () {
       if (this.signIn && this.$route.path !== '/home') {
         this.$router.push({
