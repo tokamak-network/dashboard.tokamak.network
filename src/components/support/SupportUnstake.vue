61da5cc818687511ac94eb8fa7235e68b966421b
Youngsung <sysgigigi@gmail.com>
2021-04-13 18:34:16 +0900

    Add : stake, unstake, restake support pages

--- src/components/support/SupportUnstake.vue
+++ src/components/support/SupportUnstake.vue
@@ -1,23 +1,30 @@
 <template>
   <div>
-    <div class="title">Restake</div>
-    <h3 class="subtitle">Restake TON</h3>
-    <p class="description">
-      1) Press the Unstake button. Popup will appear
-    </p>
-    <p class="description">
-      2) Writing down the quantity of TON to unstake
-    </p>
-    <p class="description">
-      3) Click ‘Unstake’ button
-    </p>
-    <p class="description">
-      4) Check the connected metamask
-    </p>
-    <p class="description">
-      * Not Withdrawable
-      : If you unstake a staked TON, it is the quantity of TON in Not Withdraw for the period set by the operator. (Default is 93046 blocks, approximately 2 weeks)
-    </p>
+    <div class="title">Un-stake</div>
+
+    <div class="image-container">
+      <img class="screenshot" src="@/assets/images/support/unstake_01.png">
+      <img class="screenshot" src="@/assets/images/support/unstake_02.png">
+    </div>
+
+    <ol>
+      <li class="description" style="margin-left:-10px; margin-bottom:10px">Press the Unstake button. Popup will appear</li>
+      <li class="description" style="margin-left:-10px; margin-bottom:10px">
+        Writing down the quantity of TON to unstake.
+      </li>
+      <li class="description" style="margin-left:-10px; margin-bottom:10px">
+        Click ‘Unstake’ button
+      </li>
+      <li class="description" style="margin-left:-10px; margin-bottom:10px">
+        Check the connected metamask
+      </li>
+    </ol>
+    <ul>
+      <li class="description" style="margin-left:-10px; margin-bottom:10px">
+        Not Withdrawable
+        : If you unstake a staked TON, it is the quantity of TON in Not Withdraw for the period set by the operator. (Default is 93046 blocks, approximately 2 weeks)
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -27,10 +34,10 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .title {
   font-family: Roboto;
-  font-size: 20px;
+  font-size: 22px;
   font-weight: 500;
   font-stretch: normal;
   font-style: normal;
@@ -39,10 +46,11 @@ export default {
   text-align: left;
   color: #2a72e5;
   margin-bottom: 20px;
+  margin-top: 40px;
 }
 .subtitle {
   font-family: Roboto;
-  font-size: 17px;
+  font-size: 18px;
   font-weight: 700;
   color: #3d495d;
 }
@@ -51,5 +59,27 @@ export default {
   text-align: left;
   font: Roboto;
   color: #3d495d;
+}
+.mini-subtitle {
+  font: Roboto;
+  font-size: 18px;
+  color: #2a72e5;
+  font-weight: normal;
+}
+.link {
+  font-family: Roboto;
+  font-size: 17px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.41;
+  letter-spacing: 0.34px;
+  text-align: left;
+  color: #2a72e5;
+}
+.image-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 }
 </style>
