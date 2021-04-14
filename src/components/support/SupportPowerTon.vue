42f7dbcf4355c8c8807448ec8ece9aa4f1de2da3
Lakmi <lakmi94@gmail.com>
2021-04-14 10:20:20 +0900

    change poerton image

--- src/components/support/SupportPowerTon.vue
+++ src/components/support/SupportPowerTon.vue
@@ -1,29 +1,18 @@
 <template>
   <div>
+     <div class="title">PowerTON</div>
+    <ol>
+        <li class="description" style="margin-left:-10px; margin-bottom:10px"><strong>Round:</strong> Current round number.</li>
+         <li class="description" style="margin-left:-10px; margin-bottom:10px"><strong>Round Reward:</strong> Total Round Reward(it is accumulated from round start).</li>
+          <li class="description" style="margin-left:-10px; margin-bottom:10px"> <strong>24 Hour:</strong> Changes in round reward compared to yesterday.</li>
+           <li class="description" style="margin-left:-10px; margin-bottom:10px"> <strong>Round Start:</strong> Round start time.</li>
+            <li class="description" style="margin-left:-10px; margin-bottom:10px"><strong>Round End:</strong> Round end time.</li>
+             <li class="description" style="margin-left:-10px; margin-bottom:10px"><strong>Rank:</strong> User Power Ranking.</li>
+              <li class="description" style="margin-left:-10px; margin-bottom:10px"><strong>Winner:</strong> Winner and reward of the previous round.</li>
+    </ol>
     <div class="image-container">
-      <img class="screenshot" src="@/assets/images/powerton_screenshot.png">
+        <img class="screenshot" src="@/assets/images/powerton_screenshot.jpg">
     </div>
-    <p class="description">
-      1) <strong>Round:</strong> Current round number.
-    </p>
-    <p class="description">
-      2) <strong>Round Reward:</strong> Total Round Reward(it is accumulated from round start).
-    </p>
-    <p class="description">
-      3) <strong>24 Hour:</strong> Changes in round reward compared to yesterday.
-    </p>
-    <p class="description">
-      4) <strong>Round Start:</strong> Round start time.
-    </p>
-    <p class="description">
-      5) <strong>Round End:</strong> Round end time.
-    </p>
-    <p class="description">
-      6) <strong>Rank:</strong> User Power Ranking.
-    </p>
-    <p class="description">
-      7) <strong>Winner:</strong> Winner and reward of the previous round.
-    </p>
   </div>
 </template>
 
@@ -34,6 +23,18 @@ export default {
 </script>
 
 <style>
+.title {
+  font-family: Roboto;
+  font-size: 22px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: 0.2px;
+  text-align: left;
+  color: #2a72e5;
+  margin-bottom: 20px;
+}
 .description {
   font-size: 17px;
   text-align: left;
@@ -49,5 +50,8 @@ export default {
 .screenshot {
     width: 100%;
     height: 100%;
+    margin-top: 70px;
+  margin-bottom: 40px;
+ box-shadow: 0 1px 4px 0 rgba(96, 97, 112, 0.16);
 }
 </style>
