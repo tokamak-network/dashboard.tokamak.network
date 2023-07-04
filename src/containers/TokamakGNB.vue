806c444de46ae6244f5a96014c9c04f1ce92c300
Jananadi Wedagedara <jananadiw@gmail.com>
2023-07-04 10:21:16 +0900

    fix: gnb width

--- src/containers/TokamakGNB.vue
+++ src/containers/TokamakGNB.vue
@@ -2,42 +2,42 @@
   <div class="gnb">
     <a
       href="https://tokamak.network/#/"
-      :style="{ width: '158px' }"
+      :style="{ width: '158px'}"
       class="link"
     >
       <div>Tokamak Network</div>
     </a>
     <a
       href="https://titan.tokamak.network/"
-      :style="{ width: '137px' }"
+      :style="{ width: '112px'}"
       class="link"
     >
       <div>L2 Mainnet</div>
     </a>
     <a
       href="https://bridge.tokamak.network/#/"
-      style="width: 168px; margin-left: -22px"
+      style="width: 136px;"
       class="link"
     >
       <div>Bridge & Swap</div>
     </a>
     <a
       href="https://simple.staking.tokamak.network/"
-      :style="{ width: '76px' }"
+      :style="{ width: '90px'}"
       class="active-link"
     >
       <div>Staking</div>
     </a>
     <a
       href="https://dao.tokamak.network/#/"
-      :style="{ width: '100px' }"
+      :style="{ width: '68px'}"
       class="link"
     >
       <div>DAO</div>
     </a>
     <a
       href="https://tonstarter.tokamak.network/"
-      :style="{ width: '100px' }"
+      :style="{ width: '110px'}"
       class="link"
     >
       <div>Launchpad</div>
